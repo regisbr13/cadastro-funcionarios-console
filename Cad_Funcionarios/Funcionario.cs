7e078a444639c64ef722c1ea6905bea02fa17123
Regis Lima <regislima1391@gmail.com>
2018-05-06 22:14:13 -0300

    Implementado cadastro de funcionarios

---
+++ Cad_Funcionarios/Funcionario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cad_Funcionarios
+{
+    class Funcionario
+    {
+        public string Cpf { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario(string cpf, string nome, double salario)
+        {
+            this.Cpf = cpf;
+            this.Nome = nome;
+            this.Salario = salario;
+        }
+    }
+}
--- Cad_Funcionarios/Program.cs
+++ Cad_Funcionarios/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+
 
 namespace Cad_Funcionarios
 {
@@ -10,6 +9,23 @@ namespace Cad_Funcionarios
     {
         static void Main(string[] args)
         {
+            string cpf, nome;
+            double salario;
+            
+            Console.Write("Quantos funcionários serão cadastrados? ");
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                Console.WriteLine("Dados do " + i + "° funcionário:");
+                Console.Write("CPF: ");
+                cpf = Console.ReadLine();
+                Console.Write("Nome: ");
+                nome = Console.ReadLine();
+                Console.Write("Salário: ");
+                salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                List<Funcionario> funcionarios = new List<Funcionario>();
+                funcionarios.Add(new Funcionario(cpf, nome, salario));
+            }
         }
     }
 }
